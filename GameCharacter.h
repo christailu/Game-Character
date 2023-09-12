66ff86712c0d578753eac709c7b7d22f00a88030
Christelle Mbemba <chmbem@cs.csis.work>
2023-09-12 12:48:46 -0500

    updated the displayDateTimeOfLastSave method

--- GameCharacter.h
+++ GameCharacter.h
@@ -1,9 +1,8 @@
 /***************************************************************/
 // Name : Christelle
 //Course : CS 355
-//Assigment 5 
-
-//date: Sept 7 2023
+//Assigment 5
+//date : sept 12 2023
 //program description:This program manages characters with various attributes such as name,
 //               max health, current health, attack points, and defense points.
 //                 The program allows characters to engage in battles, take damage,
@@ -14,24 +13,29 @@
 //*****************************************************************/
 #ifndef GAMECHARACTER_H
 #define GAMECHARACTER_H
+
 #include <iostream>
-#include <ctime>
+#include <fstream>
+#include <string>
 using namespace std;
 
+
 class GameCharacter{
-	private: 
-		string name;
-		int maxHealth =0;
-		int healthPoints =0;
-		int attackPoints = 0;
-		int defensePoints =0;
-		bool isAlive;
-		time_t lastSaveTime;
+	friend ostream& operator<<(ostream& os, const GameCharacter&  character);
 	
-	public:
-		GameCharacter (string, int, int, int, int, bool);
+	private:
+		string name;
+		int maxHealth;
+		int healthPoints;
+		int attackPoints;
+		int defensePoints;
+		bool isAlive;
 		
-		//getter method for each private member of the class
+	public:	
+	   //constructor
+		GameCharacter(string Name, int maxH, int HealthP, int AttackP, int DefenseP, bool isAlv);
+		
+		//getters method
 		string getName() const;
 		int getMaxHealth() const;
 		int getHealthPoints() const;
@@ -39,30 +43,20 @@ class GameCharacter{
 		int getDefensePoints() const;
 		bool getIsAlive() const;
 		
-		bool takeDamage(int);
-		int attack(GameCharacter&);
-		bool saveTofile(string&) const;
-		void loadFromFile(string& );
+		bool takeDamage(int damage);
+		int attack(GameCharacter& target);
 		
-		void displayTimeSinceLastSave();
+		//oveloaded operator
+		bool operator+(const GameCharacter& character);
+		bool operator<(const GameCharacter& character);
+		bool operator>(const GameCharacter& character);
 		
-		//overloaded functions
-	    // Overloaded + operator to perform addition between  attributes
-	    friend GameCharacter operator+(const GameCharacter&, const GameCharacter& ) ;
-
-	
-	    // Overloaded > operator to compare
-	    friend bool operator>(const GameCharacter& , const GameCharacter& );
-	
-	    // Overloaded < operator to compare based on health
-	    friend bool operator<(const GameCharacter& , const GameCharacter& );
-	
-	    // Overloaded << operator to display character's details
-	    friend ostream& operator<<(ostream& , const GameCharacter&);		
-			
+		void saveToFile(const string& filename) const;
+		void loadFromFile(const string& filename);
+		
+		void displayDateOfTLastSave();
 		
 		
-				
 		
 };
 
